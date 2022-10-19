50c362c4ffc7958e988c676e81b5165bb9d682ad
Matthew McIvor <matthewrmcivor@gmail.com>
2022-10-19 14:23:23 -0700

    factorial

--- src/recursion.js
+++ src/recursion.js
@@ -7,6 +7,21 @@
 // Example: 5! = 5 x 4 x 3 x 2 x 1 = 120
 // factorial(5); // 120
 var factorial = function(n) {
+  if (n < 0) {
+    return null;
+  }
+  if (n === 0) {
+    return 1;
+  }
+  var result = 1;
+  //base case
+  if (n === 1) {
+    return result;
+  }
+  if (n >= 1) {
+    return result = n * factorial(n-1);
+  }
+
 };
 
 // 2. Compute the sum of an array of integers.
